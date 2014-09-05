7a3d87fd90fa90bf4587545bd9ef83adf9a3049a
Denise Francisco <cfrancisco@wesleyan.edu>
2014-09-05 14:15:23 -0400

    attached code to python file
    :D

--- StudyBuddy.py
+++ StudyBuddy.py
@@ -1,10 +1,10 @@
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
 
 @app.route("/")
-def hello():
-  return "Hello world"
+def root():
+  return app.send_static_file('html/index.html')
 
 # Make a search for a class, and return a json object.
 @app.route("/search/<keyword>")
