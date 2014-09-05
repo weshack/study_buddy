e4a52d6b7a7b02f3c1c8a9a01fdca69c36b2ab5a
Aaron Rosen <azrosen92@gmail.com>
2014-09-05 13:04:25 -0400

    add search route

--- StudyBuddy.py
+++ StudyBuddy.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def hello():
   return "Hello world"
 
+# Make a search for a class, and return a json object.
+@app.route("/search/<keyword>")
+def search(keyword):
+	return "search page: " + keyword
+
 if __name__ == "__main__":
   app.run()
