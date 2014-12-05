c1ab11c4855082cb0e84b8b0103924190bc365d1
Aaron Rosen <azrosen92@gmail.com>
2014-12-04 20:04:22 -0500

    stuff for denise

--- study_buddy/__init__.py
+++ study_buddy/__init__.py
@@ -44,8 +44,8 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 
-csrf = CsrfProtect()
-csrf.init_app(app)
+# csrf = CsrfProtect()
+# csrf.init_app(app)
 
 print "Running app..."
 
