ab03dec1fecdc998b67eb63b2c6026db9eace87c
Aaron Rosen <azrosen92@gmail.com>
2015-01-28 16:26:18 -0500

    accessing db based on local config

--- study_buddy/init_db.py
+++ study_buddy/init_db.py
@@ -9,7 +9,7 @@ from flask.ext.login import LoginManager
 
 connection = Connection(host=app.config['DB_HOST'], port=app.config['DB_PORT'])
 connection.register([StudySession, User])
-mongo_db = connection.succor
+mongo_db = connection[app.config['DB_NAME']]
 mongo_db.study_sessions.ensure_index([('geo_location', GEOSPHERE)])
 
 login_manager = LoginManager()
